f3b675a8946ab853fa3c6e9d9cef3dd86cbe9b8f
Eric Lee <rexic999@gmail.com>
2019-09-25 21:35:47 -0500

    1.0.1

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -76,7 +76,7 @@ const mapDispatchToProps = (dispatch) => ({
   sortByDate: () => dispatch(sortByDate()),
   sortByAmount: () => dispatch(sortByAmount()),
   setStartDate: (startDate) => dispatch(setStartDate(startDate)),
-  setEndDate: (endDate) => dispatch(setEndDAte(endDate))
+  setEndDate: (endDate) => dispatch(setEndDate(endDate))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(ExpenseListFilters);
