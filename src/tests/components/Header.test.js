f6649081430a94eeb7b08506901c3f68b1db15b8
Eric Lee <rexic999@gmail.com>
2019-09-25 21:23:20 -0500

    1.0.0

---
+++ src/tests/components/Header.test.js
@@ -0,0 +1,21 @@
+import { Header } from '../../components/Header';
+import React from 'react';
+import { shallow } from 'enzyme';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({
+    adapter: new Adapter()
+})
+
+test('should render Header correctly', () => {
+    const wrapper = shallow(<Header startLogout = {() => {}}/>)
+    expect(wrapper).toMatchSnapshot();
+})
+
+test('should call startLogout on button click', () => {
+    const startLogout = jest.fn()
+    const wrapper = shallow(<Header startLogout = {startLogout} />)
+    wrapper.find('button').simulate('click')
+    expect(startLogout).toHaveBeenCalled()
+})
